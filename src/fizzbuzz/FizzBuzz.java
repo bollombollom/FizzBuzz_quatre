48b3fd34a2065a9fd7efaba80a709699d62f7f0c
BCH <bollombollom@yahoo.fr>
2023-04-21 14:12:43 +0200

    fizzbuzz(1)

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,10 @@ package fizzbuzz;
 
 public class FizzBuzz {
 
+	public static final String UN = "1";
+
+	public String fizzBuzz(int i) {
+		return UN;
+	}
+
 }
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -1,19 +1,22 @@
 package fizzbuzz;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
+	FizzBuzz fb;
+
 	@BeforeEach
 	void setUp() throws Exception {
+		fb = new FizzBuzz();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void premier_test() {
+		assertEquals("1", fb.fizzBuzz(1));
 	}
 
 }
